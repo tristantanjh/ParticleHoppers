e7a00f9d9616c13c9d2362ab1695f7b362b2d4df
tris <tristantanjh@gmail.com>
2023-06-03 14:18:46 +0800

    app.js, and ejs views created for hosting

---
+++ app.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+// const _ = require("lodash");
+const mongoose = require("mongoose");
+// const encrypt = require("mongoose-encryption"); <= encryption
+// const md5 = require("md5"); <= hashing
+// const bcrypt = require("bcryptjs"); // <= better hashing
+// const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+mongoose.connect("mongodb://127.0.0.1:27017/orbitalUserDB");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(session({
+    secret: process.env.ENCRYPTION_KEY,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+// userSchema.plugin(encrypt, {secret: process.env.ENCRYPTION_KEY, encryptedFields: ["password"]});
+
+const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.route("/")
+    .get((req, res) => {
+        res.render("home");
+    });
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+    });

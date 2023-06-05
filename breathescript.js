f5d3c84886f71948dd0db441fbaf709e95e7bc17
jon3r4de <125483479+jon3r4de@users.noreply.github.com>
2023-06-05 20:35:46 +0800

    Update breathescript.js

--- breathescript.js
+++ breathescript.js
@@ -2,20 +2,26 @@ const container = document.getElementById('container');
 const text = document.getElementById('text');
 const pointerContainer = document.querySelector('.pointer-container');
 let animationInterval;
+let redirectTimeout;
+let animationStarted = false; // Flag variable to track animation state
 
 container.addEventListener('click', () => {
-  breathAnimation(); 
+  if (!animationStarted) {
+    breathAnimation();
+    animationStarted = true;
+  }
 });
 
 const totalTime = 7500;
 const breatheTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
+const redirectTime = 7500 * 2; // Time after which to redirect in milliseconds (2 cycles)
 
 function breathAnimation() {
   text.innerText = 'Breathe In!';
   container.className = 'container grow';
 
-  //start pointer rotation 
+  // Reset the pointer rotation animation
   pointerContainer.style.animation = 'none';
   pointerContainer.offsetHeight; 
   pointerContainer.style.animation = 'rotate 7.5s linear forwards infinite';
@@ -29,6 +35,9 @@ function breathAnimation() {
     }, holdTime);
   }, breatheTime);
 
-  animationInterval = setInterval(breathAnimation, totalTime);
-} 
-
+  animationInterval = setInterval(breathAnimation, totalTime); 
+  redirectTimeout = setTimeout(() => {
+    text.innerText = 'Finished!';
+    window.location.href = 'index.html'; // Redirect to index.html
+  }, redirectTime);
+}

0ba3b758c2470aeab9f77716bef8f41149bb4e65
tris <tristantanjh@gmail.com>
2023-06-05 14:19:50 +0800

    complete login and register system

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
+// requiring required packages
+
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 // const _ = require("lodash");
 const mongoose = require("mongoose");
-// const encrypt = require("mongoose-encryption"); <= encryption
-// const md5 = require("md5"); <= hashing
-// const bcrypt = require("bcryptjs"); // <= better hashing
-// const saltRounds = 10;
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+
+// creating application using express
 
 const app = express();
 
@@ -20,7 +20,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/orbitalUserDB");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(session({
-    secret: "12431241234234123",
+    secret: process.env.ENCRYPTION_KEY,
     resave: false,
     saveUninitialized: false
 }));
@@ -33,7 +33,6 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.plugin(passportLocalMongoose);
-// userSchema.plugin(encrypt, {secret: process.env.ENCRYPTION_KEY, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
@@ -44,6 +43,45 @@ passport.deserializeUser(User.deserializeUser());
 app.route("/")
     .get((req, res) => {
         res.render("home");
+    });
+
+app.route("/login")
+    .get((req, res) => {
+        res.render("login");
+    })
+    .post((req, res) => {
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        });
+
+        req.login(user, (err) => {
+            if (err) {
+                console.log(err);
+                res.redirect("/login");
+            } else {
+                passport.authenticate("local")(req, res, () => {
+                    res.redirect("/breathe");
+                });
+            }
+        });
+    });
+
+app.route("/register")
+    .get((req, res) => {
+        res.render("register");
+    })
+    .post((req, res) => {
+        User.register({username: req.body.username}, req.body.password, (err, user) => {
+            if (err) {
+                console.log(err);
+                res.redirect("/register");
+            } else {
+                passport.authenticate("local")(req, res, () => {
+                    res.redirect("/breathe");
+                });
+            }
+        })
     });
 
 app.listen(3000, function() {

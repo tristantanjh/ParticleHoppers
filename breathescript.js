f3b56408b1d4d2ed1ed7d0118b33338d31afcadf
jon3r4de <125483479+jon3r4de@users.noreply.github.com>
2023-06-07 22:32:47 +0800

    Update breathescript.js
    added the audio in

--- breathescript.js
+++ breathescript.js
@@ -1,6 +1,8 @@
 const container = document.getElementById('container');
 const text = document.getElementById('text');
 const pointerContainer = document.querySelector('.pointer-container');
+const calmingSound = new Audio('assets/calmSound.mp3'); // Create an Audio object with the sound file
+
 let animationInterval;
 let redirectTimeout;
 let animationStarted = false; // Flag variable to track animation state
@@ -15,15 +17,17 @@ container.addEventListener('click', () => {
 const totalTime = 7500;
 const breatheTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
-const redirectTime = 7500 * 2; // Time after which to redirect in milliseconds (2 cycles)
+const redirectTime = 7500 * 5; // Time after which to redirect in milliseconds (5 cycles)
 
 function breathAnimation() {
   text.innerText = 'Breathe In!';
   container.className = 'container grow';
 
+  calmingSound.play(); // Play the calming sound
+
   // Reset the pointer rotation animation
   pointerContainer.style.animation = 'none';
-  pointerContainer.offsetHeight; 
+  pointerContainer.offsetHeight; // Trigger a reflow
   pointerContainer.style.animation = 'rotate 7.5s linear forwards infinite';
 
   setTimeout(() => {
@@ -35,7 +39,7 @@ function breathAnimation() {
     }, holdTime);
   }, breatheTime);
 
-  animationInterval = setInterval(breathAnimation, totalTime); 
+  animationInterval = setInterval(breathAnimation, totalTime); // put before breathanimation?
   redirectTimeout = setTimeout(() => {
     text.innerText = 'Finished!';
     window.location.href = 'index.html'; // Redirect to index.html

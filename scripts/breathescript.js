89b62cddf7471edfdb0b1801be8c3d6b6d533cc7
tris <tristantanjh@gmail.com>
2023-06-15 17:05:54 +0800

    Add new functionalities to "breathe" page

--- scripts/breathescript.js
+++ scripts/breathescript.js
@@ -1,16 +1,23 @@
 const container = document.getElementById('container');
 const text = document.getElementById('text');
 const pointerContainer = document.querySelector('.pointer-container');
-const calmingSound = new Audio('/assets/calmSound.mp3'); // Create an Audio object with the sound file
+const calmingSound = new Audio('assets/calmSound.mp3'); // Create an Audio object with the sound file
+const breathsText = document.querySelector(".breaths-text");
+let breathsLeft = 3; 
 
 let animationInterval;
 let redirectTimeout;
+let hold;
+let breathe;
 let animationStarted = false; // Flag variable to track animation state
 
 container.addEventListener('click', () => {
+  //breathsText.innerText = breathsLeft;
   if (!animationStarted) {
-    breathAnimation();
+    start();
     animationStarted = true;
+  } else {
+    pause();
   }
 });
 
@@ -20,28 +27,75 @@ const holdTime = totalTime / 5;
 const redirectTime = 7500 * 5; // Time after which to redirect in milliseconds (5 cycles)
 
 function breathAnimation() {
+  breathsText.innerText = breathsLeft;
   text.innerText = 'Breathe In!';
   container.className = 'container grow';
 
-  calmingSound.play(); // Play the calming sound
-
-  // Reset the pointer rotation animation
-  pointerContainer.style.animation = 'none';
   pointerContainer.offsetHeight; // Trigger a reflow
   pointerContainer.style.animation = 'rotate 7.5s linear forwards infinite';
+  //pointerContainer.style.animation = 'none'; can use to reset
 
-  setTimeout(() => {
+   hold = setTimeout(() => {
     text.innerText = 'Hold';
-
-    setTimeout(() => {
+    
+     breathe = setTimeout(() => {
       text.innerText = 'Breathe Out!';
       container.className = 'container shrink';
     }, holdTime);
   }, breatheTime);
 
-  animationInterval = setInterval(breathAnimation, totalTime); // put before breathanimation?
-  redirectTimeout = setTimeout(() => {
-    text.innerText = 'Finished!';
-    window.location.href = '/quote'; // Redirect to quote
-  }, redirectTime);
+  breathsLeft--;
+  
+  if (breathsLeft <= -1) {
+    redirect();
+  }
+
+}
+
+function start() {
+  calmingSound.play();
+
+  text.innerText = 'starting soon...';
+
+  setTimeout(() => {
+    text.innerText = '3';
+  }, (totalTime - 3000));
+
+  setTimeout(() => {
+    text.innerText = '2';
+  }, (totalTime - 2000));
+
+  setTimeout(() => {
+    text.innerText = '1';
+  }, totalTime - 1000);
+
+  animationInterval = setInterval(breathAnimation, totalTime);
+  // Consider putting breathsLeft--
+}
+
+// Rest of the code remains the same
+
+function pause() {
+  breathsLeft++;
+  container.className = 'container'; // Reset the class to its initial state
+  pointerContainer.style.animation = 'none'; //can use to reset
+  text.innerText = 'Ready to continue?';
+  clearInterval(animationInterval);
+  clearTimeout(hold);
+  clearTimeout(breathe);
+  animationStarted = false;
+  calmingSound.pause();
+}
+
+function redirect() {
+  pointerContainer.style.animation = 'none'; //can use to reset
+  text.innerText = 'finished';
+  clearTimeout(hold); //cleartimeouts not working
+  clearTimeout(breathe);
+  animationStarted = false;
+  //calmingSound.pause();
+
+  setTimeout(() => {
+    window.location.href = '/quote'; // Redirect to index.html
+  }, 3000);
 }

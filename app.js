4f6a66c779583f595f07f15f51fe219ed90627bf
tris <tristantanjh@gmail.com>
2023-06-15 16:40:00 +0800

    Implement route handling for 404 error pages

--- app.js
+++ app.js
@@ -115,6 +115,10 @@ app.route("/quote")
         }
     });
 
+app.get('*', (req, res) => {
+    res.status(404).render('404');
+});
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
     });

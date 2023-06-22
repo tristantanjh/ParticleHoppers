4ec6580e818fc6eefdaa89891ceacb401b2864da
tris <tristantanjh@gmail.com>
2023-06-22 18:12:22 +0800

    Change text in home route if logged in

--- routes/home.js
+++ routes/home.js
@@ -4,6 +4,7 @@ const { User, passport } = require('../user');
 
 const defaultMainText = "It's okay to not be okay."
 const defaultSubText = "We’re here to support and allow everyone to feel more at ease in their daily lives by providing support and guidance along the way. No matter what you’re going through, we’re here for you."
+const loginSubText = "Thank you for allowing us to support your mental health journey."
 
 // Renders home page
 router.get("/", (req, res) => {
@@ -12,14 +13,27 @@ router.get("/", (req, res) => {
         User.findOne({ _id: userId })
             .then((user) => {
                 const username = user.username;
-                console.log(username);
+                res.render("home", { 
+                    pageTitle: "Home", 
+                    mainText: "Welcome back, " + username,
+                    subText: loginSubText,
+                    beginOrContinue: "Take a Breather",
+                    loginOrQuoteRoute: "/quote",
+                    loginOrQuoteText: "Daily Quote"
+                });
             })
             .catch((error) => {
                 console.error('Error retrieving user:', error);
             });
-        res.render("home", { pageTitle: "Home" });
     } else {
-        res.render("home", { pageTitle: "Home" });
+        res.render("home", { 
+            pageTitle: "Home",  
+            mainText: defaultMainText,
+            subText: defaultSubText,
+            beginOrContinue: "Begin Now",
+            loginOrQuoteRoute: "/login",
+            loginOrQuoteText: "Login"
+        });
     }
 });
 

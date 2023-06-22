7f9f1b426089a8393e104091d46747c943251a68
tris <tristantanjh@gmail.com>
2023-06-22 17:29:44 +0800

    Fix Bugs

--- routes/logout.js
+++ routes/logout.js
@@ -4,7 +4,9 @@ const router = express.Router();
 // Logs user out and redirects to home page
 router.get("/", (req, res) => {
     req.logout((err) => {
-        console.log(err);
+        if (err) {
+            console.log(err);
+        }
     });
     res.redirect("/");
 });

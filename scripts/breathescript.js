2bb1c27d17f532af118fb0558f517b8823f5caba
jon3r4de <125483479+jon3r4de@users.noreply.github.com>
2023-06-24 01:49:05 +0800

    Update breathescript.js
    modularised the code further, and fixed a bug where user could add in number of breathe cycles if variable exhale was true

--- scripts/breathescript.js
+++ scripts/breathescript.js
@@ -1,129 +1,121 @@
+// Module for managing the breathing animation
+function BreathingAnimation(containerElement, textElement, pointerContainerElement, breathsTextElement, calmingSound) {
+  const totalTime = 7500;
+  const breatheTime = (totalTime / 5) * 2;
+  const holdTime = totalTime / 5;
+  const redirectTime = 7500 * 5; // Time after which to redirect in milliseconds (5 cycles)
+
+  let animationInterval;
+  let holdTimeout;
+  let breatheTimeout;
+  let threeWaitTimeout;
+  let twoWaitTimeout;
+  let oneWaitTimeout;
+  let animationStarted = false;
+  let exhaleStart = false;
+  let breathsLeft = 4;
+
+  function breathAnimation() {
+    exhaleStart = false;
+    breathsTextElement.innerText = breathsLeft;
+    textElement.innerText = 'Breathe In!';
+    containerElement.className = 'container grow';
+
+    pointerContainerElement.offsetHeight; // Trigger a reflow
+    pointerContainerElement.style.animation = 'rotate 7.5s linear forwards infinite';
+
+    holdTimeout = setTimeout(() => {
+      textElement.innerText = 'Hold';
+
+      breatheTimeout = setTimeout(() => {
+        textElement.innerText = 'Breathe Out!';
+        containerElement.className = 'container shrink';
+        exhaleStart = true;
+        breathsLeft--;
+      }, holdTime);
+    }, breatheTime);
+
+    if (breathsLeft <= 0) {
+      redirect();
+      containerElement.removeEventListener('click', clickHandler);
+    }
+  }
+
+  function start() {
+    calmingSound.play();
+
+    if (exhaleStart === false) {
+      breathsLeft--;
+    }
+
+    exhaleStart = false; //prevent any addition of clicks if exhale start is true initially
+
+    textElement.innerText = 'starting soon...';
+
+    threeWaitTimeout = setTimeout(() => {
+      textElement.innerText = '3';
+    }, totalTime - 3000);
+
+    twoWaitTimeout = setTimeout(() => {
+      textElement.innerText = '2';
+    }, totalTime - 2000);
+
+    oneWaitTimeout = setTimeout(() => {
+      textElement.innerText = '1';
+    }, totalTime - 1000);
+
+    animationInterval = setInterval(breathAnimation, totalTime);
+  }
+
+  function pause() {
+    breathsLeft++;
+    containerElement.className = 'container';
+    pointerContainerElement.style.animation = 'none';
+    textElement.innerText = 'Ready to continue?';
+    clearInterval(animationInterval);
+    clearTimeout(holdTimeout);
+    clearTimeout(breatheTimeout);
+    clearTimeout(threeWaitTimeout);
+    clearTimeout(twoWaitTimeout);
+    clearTimeout(oneWaitTimeout);
+    animationStarted = false;
+    calmingSound.pause();
+    calmingSound.load();
+  }
+
+  function redirect() {
+    pointerContainerElement.style.animation = 'none';
+    textElement.innerText = 'finished';
+    clearTimeout(holdTimeout);
+    clearTimeout(breatheTimeout);
+    animationStarted = false;
+
+    setTimeout(() => {
+      window.location.href = '/quote'; // Redirect to index.html
+    }, 3000);
+  }
+
+  function clickHandler() {
+    if (!animationStarted) {
+      start();
+      animationStarted = true;
+    } else {
+      pause();
+    }
+  }
+
+  containerElement.addEventListener('click', clickHandler);
+
+  return {
+    // Expose any public methods or properties here (if needed)
+  };
+}
+
+// Usage example
 const container = document.getElementById('container');
 const text = document.getElementById('text');
 const pointerContainer = document.querySelector('.pointer-container');
-const calmingSound = new Audio('assets/calmSound.mp3'); // Create an Audio object with the sound file
-const breathsText = document.querySelector(".breaths-text");
-let breathsLeft = 4; //set as 4 cos we want 3 cycles, but because of  
+const breathsText = document.querySelector('.breaths-text');
+const calmingSound = new Audio('assets/calmSound.mp3');
 
-let animationInterval;
-let redirectTimeout;
-let hold;
-let breathe;
-let threeWait;
-let twoWait;
-let oneWait;
-let animationStarted = false; // Flag variable to track animation state
-let exhaleStart = false;
-
-/*container.addEventListener('click', () => {
-  //breathsText.innerText = breathsLeft;
-  if (!animationStarted) {
-    start();
-    animationStarted = true;
-  } else {
-    pause();
-  }
-});*/
-
-const clickHandler = () => {
-  //breathsText.innerText = breathsLeft;
-  if (!animationStarted) {
-    start();
-    animationStarted = true;
-  } else {
-    pause();
-  }
-};
-
-container.addEventListener('click', clickHandler);
-
-const totalTime = 7500;
-const breatheTime = (totalTime / 5) * 2;
-const holdTime = totalTime / 5;
-const redirectTime = 7500 * 5; // Time after which to redirect in milliseconds (5 cycles)
-
-function breathAnimation() {
-  exhaleStart = false;
-  breathsText.innerText = breathsLeft;
-  text.innerText = 'Breathe In!';
-  container.className = 'container grow';
-
-  pointerContainer.offsetHeight; // Trigger a reflow
-  pointerContainer.style.animation = 'rotate 7.5s linear forwards infinite';
-  //pointerContainer.style.animation = 'none'; can use to reset
-
-   hold = setTimeout(() => {
-    text.innerText = 'Hold';
-    
-     breathe = setTimeout(() => {
-      text.innerText = 'Breathe Out!';
-      container.className = 'container shrink';
-      exhaleStart = true;
-      breathsLeft--;
-    }, holdTime);
-  }, breatheTime);
-
-  
-  
-  if (breathsLeft <= 0) {
-    redirect();
-    container.removeEventListener('click', clickHandler);
-  }
-
-}
-
-function start() {
-  calmingSound.play();
-
-  if (exhaleStart === false) {
-    breathsLeft--;
-  }
-
-  text.innerText = 'starting soon...';
-
-  threeWait = setTimeout(() => {
-    text.innerText = '3';
-  }, (totalTime - 3000));
-
-  twoWait = setTimeout(() => {
-    text.innerText = '2';
-  }, (totalTime - 2000));
-
-  oneWait = setTimeout(() => {
-    text.innerText = '1';
-  }, totalTime - 1000);
-
-  animationInterval = setInterval(breathAnimation, totalTime);
-  // Consider putting breathsLeft--
-}
-
-// Rest of the code remains the same
-
-function pause() {
-  breathsLeft++;
-  container.className = 'container'; // Reset the class to its initial state
-  pointerContainer.style.animation = 'none'; //can use to reset
-  text.innerText = 'Ready to continue?';
-  clearInterval(animationInterval);
-  clearTimeout(hold);
-  clearTimeout(breathe);
-  clearTimeout(threeWait);
-  clearTimeout(twoWait);
-  clearTimeout(oneWait);
-  animationStarted = false;
-  calmingSound.pause();
-  calmingSound.load(); //restarts audio
-}
-
-function redirect() {
-  pointerContainer.style.animation = 'none'; //can use to reset
-  text.innerText = 'finished';
-  clearTimeout(hold); //cleartimeouts not working
-  clearTimeout(breathe);
-  animationStarted = false;
-  //calmingSound.pause();
-
-  setTimeout(() => {
-    window.location.href = '/quote'; // Redirect to index.html
-  }, 3000);
-}
+const breathingAnimation = BreathingAnimation(container, text, pointerContainer, breathsText, calmingSound);

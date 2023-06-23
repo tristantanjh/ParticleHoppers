ab536fbd583df0b95cea197aba51f5f629e69a22
tris <tristantanjh@gmail.com>
2023-06-23 23:09:44 +0800

    Check for navigation to all routes from home page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,35 @@
 describe('Visit Homepage', () => {
-  it('Visits the homepage', () => {
-    cy.visit('localhost:3000')
+  beforeEach(() => {
+    cy.visit('localhost:3000');
   })
+
+  it('should navigate to the login page from the first button', () => {
+    cy.get('#login-btn').click();
+    cy.url().should('include', '/login');
+  })
+
+  it('should navigate to the login page from the second button', () => {
+    cy.get('#begin-btn').click(); 
+    cy.url().should('include', '/login');
+  });
+
+  it('should navigate to the mission section on homepage from top navbar', () => {
+    cy.get('.mission-topnav').click();
+    cy.url().should('include', '#mission');
+  });
+
+  it('should navigate to the top of homepage from bottom navbar', () => {
+    cy.get('.home-botnav').click();
+    cy.url().should('include', '#main');
+  });
+
+  it('should navigate to the mission section on homepage from bottom navbar', () => {
+    cy.get('.mission-botnav').click();
+    cy.url().should('include', '#mission');
+  });
+
+  it('should navigate to the about page from bottom navbar', () => {
+    cy.get('.about-botnav').click();
+    cy.url().should('include', '/about');
+  });
 })

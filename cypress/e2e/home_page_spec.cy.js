a41327dc115a73a50393121e357f240f09fa3bae
tris <tristantanjh@gmail.com>
2023-06-24 17:40:11 +0800

    Modify test cases

--- cypress/e2e/home_page_spec.cy.js
+++ cypress/e2e/home_page_spec.cy.js
@@ -1,39 +1,46 @@
 describe('Homepage', () => {
-  it('successfully loads homepage', () => {
-    cy.visit('/');
-  })
+	// Test case: Successfully loads homepage
+	it('successfully loads homepage', () => {
+		cy.visit('/');
+	})
 
-  beforeEach(() => {
-    cy.visit('/');
-  })
+	beforeEach(() => {
+		cy.visit('/');
+	})
 
-  it('should navigate to the login page from the first button', () => {
-    cy.get('#login-btn').click();
-    cy.url().should('include', '/login');
-  })
+	// Test case: Navigate to the login page from the login button on top navbar
+	it('should navigate to the login page from the login button on top navbar', () => {
+		cy.get('#login-btn').click();
+		cy.url().should('include', '/login');
+	})
 
-  it('should navigate to the login page from the second button', () => {
-    cy.get('#begin-btn').click(); 
-    cy.url().should('include', '/login');
-  });
+	// Test case: Navigate to the login page from 'begin' button
+	it("should navigate to the login page from 'begin' button", () => {
+		cy.get('#begin-btn').click(); 
+		cy.url().should('include', '/login');
+	});
 
-  it('should navigate to the mission section on homepage from top navbar', () => {
-    cy.get('.mission-topnav').click();
-    cy.url().should('include', '#mission');
-  });
+	// Test case: Navigate to the mission section on homepage from top navbar
+	it('should navigate to the mission section on homepage from top navbar', () => {
+		cy.get('.mission-topnav').click();
+		cy.url().should('include', '#mission');
+	});
 
-  it('should navigate to the top of homepage from bottom navbar', () => {
-    cy.get('.home-botnav').click();
-    cy.url().should('include', '#main');
-  });
+	// Test case: Navigate to the top of homepage from bottom navbar
+	it('should navigate to the top of homepage from bottom navbar', () => {
+		cy.get('.home-botnav').click();
+		cy.url().should('include', '#main');
+	});
 
-  it('should navigate to the mission section on homepage from bottom navbar', () => {
-    cy.get('.mission-botnav').click();
-    cy.url().should('include', '#mission');
-  });
+	// Test case: Navigate to the mission section on homepage from bottom navbar
+	it('should navigate to the mission section on homepage from bottom navbar', () => {
+		cy.get('.mission-botnav').click();
+		cy.url().should('include', '#mission');
+	});
 
-  it('should navigate to the about page from bottom navbar', () => {
-    cy.get('.about-botnav').click();
-    cy.url().should('include', '/about');
-  });
+	// Test case: Navigate to the about page from bottom navbar
+	it('should navigate to the about page from bottom navbar', () => {
+		cy.get('.about-botnav').click();
+		cy.url().should('include', '/about');
+	});
 })

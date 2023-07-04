563e084d55ec8ef4d74f77b0ee67ee51f7535853
tris <tristantanjh@gmail.com>
2023-07-04 19:12:24 +0800

    Create Testing Stub for Journal Entry

--- routes/journal.js
+++ routes/journal.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// Renders About page
+// Renders Journal page
 router.get("/", (req, res) => {
     res.render("journal", { pageTitle: "Journal"});
 });

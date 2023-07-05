fc900f4d4487985563cfd7eae10ef1d57a85c1c3
tris <tristantanjh@gmail.com>
2023-07-05 15:53:06 +0800

    Show today's journal entry after publishing

--- routes/journal.js
+++ routes/journal.js
@@ -4,11 +4,31 @@ const { User } = require('../models/user');
 const { JournalEntry } = require('../models/journalEntry');
 
 // Renders Journal page
-router.get("/", (req, res) => {
-    if (req.isAuthenticated()) {
-        res.render("journal", { pageTitle: "Journal"});
-    } else {
+router.get("/", async (req, res) => {
+    try {
+      if (req.isAuthenticated()) {
+        const currentUser = req.user; // Access the authenticated user from req.user
+  
+        // Check if the user has already submitted an entry today
+        const today = new Date().setHours(0, 0, 0, 0);
+        const journalEntry = await JournalEntry.findOne({
+          user: currentUser._id,
+          createdAt: { $gte: today },
+        });
+  
+        if (journalEntry) {
+          // Render the existing journal entry
+          res.render("journal", { pageTitle: "Journal", journalEntry });
+        } else {
+          // Render the form to submit a new entry
+          res.render("journal", { pageTitle: "Journal", journalEntry: null });
+        }
+      } else {
         res.redirect("/login");
+      }
+    } catch (error) {
+      console.error(error);
+      res.redirect("/login");
     }
 });
 
@@ -34,5 +54,28 @@ router.post('/', async (req, res) => {
       console.error(error);
     }
 });
+
+router.post("/edit", async (req, res) => {
+    try {
+      const currentUser = req.user;
+      const { title, content } = req.body;
+  
+      // Find and update the journal entry for today
+      const today = new Date().setHours(0, 0, 0, 0);
+      const journalEntry = await JournalEntry.findOneAndUpdate(
+        {
+          user: currentUser._id,
+          createdAt: { $gte: today },
+        },
+        { title, content },
+        { new: true }
+      );
+  
+      res.redirect("/journal");
+    } catch (error) {
+      console.error(error);
+      res.redirect("/login");
+    }
+  });
 
 module.exports = router;
